f016c25524a54943d14992d64dd70487b7fcfcf0
Dileep Nandanam <dileep@Dileeps-MacBook-Air.local>
2019-06-23 18:21:39 -0700

    adding functionalities to set access rights

--- app/controllers/dynamic_acls_controller.rb
+++ app/controllers/dynamic_acls_controller.rb
@@ -1,15 +1,30 @@
 class DynamicAclsController < ActionController::Base
+  skip_before_action :verify_authenticity_token
   def index
-    @object = params[:type].camelize.constantize
-    @acls = Acl.where(object_type: params[:type]).all
+    @object = params[:object_type].camelize.constantize
+    @acls = Acl.where(object_type: @object.name).all
+    @rules = rules
     render inline: ERB.new(File.open("/#{__FILE__.split('controllers').first}views/index.erb").read).result(binding), type: 'text/html'
   end
 
   def set
+    @acl = Acl.where(params.require(:acl).permit(:object_type, :user_type, :state, :action, :user_id)).first
     if @acl.present?
-      @acl = Acl.where(user_type: params[:user_type], user_id: params[:user_id], action: params[:action], object_type: params[:object_type], status: params[:status]).first_or_create
+      Acl.destroy(@acl.id)
     else
-      @acl.delete
+      @acl = Acl.create(params.require(:acl).permit(:object_type, :user_type, :state, :action, :user_id))
     end
+
+    @object = params[:acl][:object_type].camelize.constantize
+    @acls = Acl.where(object_type: @object.name).all
+    
+    render inline: rules
+  end
+
+  protected
+
+  def rules
+    @rules = @acls.sort_by(&:user_type).map{|acl| "#{acl.user_type} can #{acl.action} #{acl.state} #{acl.object_type}"}.join('</br>')
+    @rules = @rules.blank? ? 'Good, nobody is permitted to do anything' : @rules
   end
 end
